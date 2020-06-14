49ed3072530a0098a27a116e3ca04cff4a1c30cb
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-14 19:27:38 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -2,16 +2,16 @@ def second_challenge
   epic_tragedy = {
     
     :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
       }, 
       
     :capulet => {
-      :patriarch => {}
-      :matriarch => {}
-      :heroine => {}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends => []
       }
   }
